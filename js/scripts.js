7de28c3eb46949caa6468e129572a20d434fca1a
Stewart ilondanga <stilondanga@gmail.com>
2019-02-16 09:58:38 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
-
+/*
 // callback for card click
 function flipCard(self) {
   self.classList.toggle('card--flipped');
